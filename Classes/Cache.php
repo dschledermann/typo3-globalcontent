5d4e7211b2af4feec58e2a95a1c63eb63c1a200b
Daniel Schledermann <daniel@linkfactory.dk>
2018-01-15 14:41:19 +0100

    [TASK] Upgrade cache declarations

--- Classes/Cache.php
+++ Classes/Cache.php
@@ -2,27 +2,27 @@
 
 namespace Linkfactory\Globalcontent;
 
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 class Cache {
 
 	/**
 	 * Get instance of TYPO3 Caching Framework.
-	 * 
+	 *
 	 * @return object TYPO3 Caching Framework.
 	 */
 	public static function getTYPO3CacheInstance() {
 		$cacheIdentifier = "globalcontent_cache";
 
 		// Initialize TYPO3 cache caching framework.
-        \TYPO3\CMS\Core\Cache\Cache::initializeCachingFramework();
+		if (version_compare(TYPO3_version, '8.0', '<')) {
+			\TYPO3\CMS\Core\Cache\Cache::initializeCachingFramework();
+		}
+
 		try {
-			$cacheInstance = $GLOBALS['typo3CacheManager']->getCache($cacheIdentifier);
+			$cacheInstance = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Cache\\CacheManager')->getCache($cacheIdentifier);
 		} catch (\TYPO3\CMS\Core\Cache\Exception\NoSuchCacheException $e) {
-			$cacheInstance = $GLOBALS['typo3CacheFactory']->create(
-					$cacheIdentifier,
-					$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cacheIdentifier]['frontend'],
-					$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cacheIdentifier]['backend'],
-					$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cacheIdentifier]['options']
-			);
+			$cacheInstance = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Cache\\CacheManager')->createCache($cacheIdentifier);
 		}
 		return $cacheInstance;
 	}
